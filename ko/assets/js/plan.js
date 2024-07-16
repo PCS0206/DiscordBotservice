14f86d505e9fe227662a9fe3eaaf19de5eb86600
PCS0206 <127528087+PCS0206@users.noreply.github.com>
2024-07-16 23:05:40 +0800

    Update plan.js

--- ko/assets/js/plan.js
+++ ko/assets/js/plan.js
@@ -9,7 +9,7 @@ document.getElementById('calculate').addEventListener('submit', function(event) 
     const webhookUrl = 'https://discord.com/api/webhooks/1262784159185633281/oimiHfF__Z4UjNokfHFWhtKclbbgZrxMF3CnVFdCuW6v2wMIoyBzddbQTlwNiS7trhLf'; // 여기에 자신의 웹훅 URL을 입력하세요
 
     const data = {
-        content: `New form submission:\nName: ${name}\nEmail: ${email}\nSubject: ${subject}\nCategory: ${category}`
+        content: `새 상담 문의:\nName: ${name}\nEmail: ${email}\nSubject: ${subject}\nCategory: ${category}`
     };
 
     fetch(webhookUrl, {
@@ -21,7 +21,7 @@ document.getElementById('calculate').addEventListener('submit', function(event) 
     })
     .then(response => {
         if (response.ok) {
-            alert('Form submitted successfully!');
+            alert('성공적으로 문의가 전송되었습니다! 영업일 기준 7일 이내 이메일로 연락 드리겠습니다!');
         } else {
             console.error('Failed to submit form:', response.status, response.statusText);
             alert('Failed to submit form. Check console for details.');
