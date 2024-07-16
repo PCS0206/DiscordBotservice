85774235fead469791f753289d7afafbcc34bb35
PCS0206 <127528087+PCS0206@users.noreply.github.com>
2024-07-16 22:54:51 +0800

    Update plan.js

--- ko/assets/js/plan.js
+++ ko/assets/js/plan.js
@@ -6,7 +6,7 @@ document.getElementById('calculate').addEventListener('submit', function(event) 
     const subject = document.getElementById('subject').value;
     const category = document.getElementById('chooseOption').value;
 
-    const webhookUrl = 'https://discord.com/api/webhooks/your-webhook-id/your-webhook-token'; // 여기에 자신의 웹훅 URL을 입력하세요
+    const webhookUrl = 'https://discord.com/api/webhooks/1262784159185633281/oimiHfF__Z4UjNokfHFWhtKclbbgZrxMF3CnVFdCuW6v2wMIoyBzddbQTlwNiS7trhLf'; // 여기에 자신의 웹훅 URL을 입력하세요
 
     const data = {
         content: `New form submission:\nName: ${name}\nEmail: ${email}\nSubject: ${subject}\nCategory: ${category}`
